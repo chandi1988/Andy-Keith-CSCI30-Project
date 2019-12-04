ba190e43afc954f905fabd6473c66dc008febe2e
Keithy1007 <keith.santos@obf.ateneo.edu>
2019-12-04 14:54:43 +0800

    I updated it now it can change and check names/age

--- personclass.py
+++ personclass.py
@@ -6,14 +6,14 @@ class Person:
 		self.sex = sex 
 		
 personlist = [] 
-personlist.append(Person("Carlana", 20, "Female"))
-personlist.append(Person("Bobby", 18, "Male"))
-personlist.append(Person("Sasha", 25, "Female"))
-personlist.append(Person("Keithy", 18, "Male"))
-
+personlist.append(Person("Carlana", 20, "true"))
+personlist.append(Person("Bobby", 18, "false"))
+personlist.append(Person("Sasha", 25, "false"))
+personlist.append(Person("Keithy", 18, "true"))
+"""
 print(personlist[0].name)
 
-look = 'Female'
+look = 'false'
 for person in personlist:
     if look in person.sex:
         # title() capitalizes the job's first letter
@@ -22,3 +22,33 @@ for person in personlist:
 for person in personlist:
     if 18 == person.age:
         print( "%s: \"%s\"" % (person.name, person.age))
+"""
+#getting age and name, changing age and name from an array of classes
+while True: 
+	p = input()
+	q =  p.split(" ",2)
+	if p == "end":
+		break
+	elif q[0] == "changename":
+		for person in personlist:
+			if q[1] in person.name:
+				print( "%s changed to %s" % (person.name, q[2]))
+				person.name = q[2]
+			else:
+				print("there aint nobody named " + q[1])
+	elif q[0] == "changeage":
+		for person in personlist:
+			if q[1] in person.name:
+				print( "%s\'s age is changed from %s to %s" % (person.name, person.age, q[2]))
+				person.age = int(q[2])
+			else:
+				print("there aint nobody aged " + q[1])
+	elif q[0] == "checkage":
+		for person in personlist:
+			if int(q[1]) == person.age:
+				print( "%s: \"%s\"" % (person.name, person.age))
+			else:
+				print("there aint nobody aged " + q[1]) 
+	elif q[0] == "enumnames":			
+		for person in personlist:		
+			print(person.name)
