ebb52e849ebf6d1805b62c3f80343a0d46bca172
Keithy1007 <keith.santos@obf.ateneo.edu>
2019-12-09 17:58:24 +0800

    trying again, final files

--- personclass.py
+++ personclass.py
@@ -1,4 +1,4 @@
-"""person class and searching example"""
+"""this is our person class"""
 personArray = []
 class Person:
 	def __init__(self, name, age, sex):
@@ -9,8 +9,7 @@ class Person:
 		self.right = None
 		self.parent = None
 		self.gen = 1
-	def setLeft(self, child):
-		self.left = child
+		self.isroot = False
 
 def addtoPersonArray(name, age, sex):
 	personArray.append(Person(name, age, sex))
@@ -18,69 +17,31 @@ def addtoPersonArray(name, age, sex):
 def returnPerson():
 	return personArray
 
+def findPerson(name):
+    for i in personArray:
+        if name == i.name:
+            return i
+			
+def changeName(origname, newname):
+	if findPerson(newname) == None:
+		findPerson(origname).name = newname
+		print(origname + "'s name has been changed to " + newname)
+	else: 
+		print("Error: name " + newname + " already in use. Please change to another name.")
+		
+def changeAge(origname, newage):
+	for i in personArray:
+		if origname == i.name:
+			i.age = newage
+			print(origname + "'s age has been changed to " + newage)
 
+def changeSex(origname, newsex):
+	for i in personArray:
+		if origname == i.name:
+			if newsex.lower() == "false":
+				i.sex = False
+				print(origname + "'s sex has been changed to female")
+			elif newsex.lower() == "true":
+				i.sex = True
+				print(origname + "'s sex has been changed to male")
 
-
-"""getting age and name, changing age and name from an array of classes in cmd"""
-"""
-while True:
-	p = input()
-	q =  p.split(" ",2)
-	existence = False
-	if p == "bye":
-		break
-	elif q[0] == "count":
-		print(len(personlist))
-	elif q[0] == "age":
-		for person in personlist:
-			if q[1] in person.name:
-				print( "%s is %s years old" % (person.name, person.age))
-				existence = True
-		if existence == False:
-			print("there aint nobody named " + q[1])
-		existence = False
-	elif q[0] == "sex":
-		for person in personlist:
-			if q[1] in person.name:
-				if person.sex == True:
-					print( "%s is Male" % (person.name))
-				elif person.sex == False:
-					print( "%s is Female" % (person.name))
-				existence = True
-		if existence == False:
-			print("there aint nobody named " + q[1])
-		existence = False
-	elif q[0] == "edit-age":
-		for person in personlist:
-			if q[1] in person.name:
-				print( "%s changed to %s" % (person.name, q[2]))
-				person.name = q[2]
-				existence = True
-		if existence == False:
-			print("there aint nobody named " + q[1])
-		existence = False
-	elif q[0] == "edit-age":
-		for person in personlist:
-			if q[1] in person.name:
-				print( "%s\'s age is changed from %s to %s" % (person.name, person.age, q[2]))
-				person.age = int(q[2])
-				existence = True
-		if existence == False:
-			print("there aint nobody aged " + q[1])
-		existence = False
-	elif q[0] == "edit-sex":
-		for person in personlist:
-			if q[1] in person.name:
-				print( "%s\'s sex is changed" % (person.name))
-				if (q[2]) == "False":
-					person.sex = False
-				elif q[2] == "True":
-					person.sex = True
-				existence = True
-		if existence == False:
-			print("there aint nobody named " + q[1])
-		existence = False
-	elif q[0] == "enumdeets":
-		for person in personlist:
-			print("%s - %s" % (person.name, person.age))
-"""
---
+++ treestarter.py
@@ -0,0 +1,109 @@
+from personclass import *
+from relationclass import *
+from testulit import *
+
+while True:
+	ipt = input(">")
+	if ipt.lower() == "bye":
+		print("Goodbye!")
+		break
+	elif ipt.split()[0].lower() == "load":
+		try:
+			returnPerson().clear()
+			getLinkIn().clear()
+		
+			FamTreeLoad(ipt.split()[1])
+			makeTree()
+			if len(returnPerson()) != 0:
+				print(ipt.split()[1] + ".ft has been loaded!!!")
+		except:
+			if getError() == True:
+				editError(False)
+				pass
+			else:
+				print("Error: file does not exist")
+	elif ipt.split()[0].lower() == "save":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		else:
+			saveFamTree(ipt.split()[1])
+			print("Family Tree has been updated!")
+	elif ipt.split()[0].lower() == "count":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		else:
+			print(len(returnPerson()))
+	elif ipt.split()[0].lower() == "age":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		elif findPerson(ipt.split()[1]) == None:
+			print("Error: no such person as " + ipt.split()[1] + " in " + getFamName() +" family")
+		else:
+			print(findPerson(ipt.split()[1]).age)
+	elif ipt.split()[0].lower() == "sex":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		elif findPerson(ipt.split()[1]) == None:
+			print("Error: no such person as " + ipt.split()[1] + " in " + getFamName() +" family")
+		else:
+			if findPerson(ipt.split()[1]).sex == True:
+				print("Male")
+			elif findPerson(ipt.split()[1]).sex == False:
+				print("Female")
+	elif ipt.split()[0].lower() == "ancestors":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		elif findPerson(ipt.split()[1]) == None:
+			print("Error: no such person as " + ipt.split()[1] + " in " + getFamName() +" family")
+		else:
+			listAncestors(ipt.split()[1]) 
+	elif ipt.split()[0].lower() == "descendants":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		elif findPerson(ipt.split()[1]) == None:
+			print("Error: no such person as " + ipt.split()[1] + " in " + getFamName() +" family")
+		else:
+			returnDescendants(ipt.split()[1])
+	elif ipt.split()[0].lower() == "edit-name":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		elif findPerson(ipt.split()[1]) == None:
+			print("Error: no such person as " + ipt.split()[1] + " in " + getFamName() +" family")
+		else:
+			changeName(ipt.split()[1],ipt.split()[2])
+	elif ipt.split()[0].lower() == "edit-age":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		elif findPerson(ipt.split()[1]) == None:
+			print("Error: no such person as " + ipt.split()[1] + " in " + getFamName() +" family")
+		else:
+			changeAge(ipt.split()[1],ipt.split()[2])
+	elif ipt.split()[0].lower() == "edit-sex":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		elif findPerson(ipt.split()[1]) == None:
+			print("Error: no such person as " + ipt.split()[1] + " in " + getFamName() +" family")
+		else:
+			changeSex(ipt.split()[1],ipt.split()[2])
+	elif ipt.split()[0].lower() == "relationship":
+		if len(returnPerson()) == 0:
+			print("Error: a family tree must be loaded in the current session first")
+		else:
+			try:
+				if ipt.split()[1] == ipt.split()[2]:
+					print("Error: inputs must be distinct")
+				else: 
+					print(relationship(ipt.split()[1], ipt.split()[2]))
+			except:
+				print("Error: input(s) are nonexistent, please fix and try again")
+	else: 
+		print("Error: unrecognized keyword \"" + ipt.split()[0] + "\"")
+	
+	
+	
+	
+	
+	
+	
+	
+	
